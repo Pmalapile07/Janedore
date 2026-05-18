82588c71ba384f64a7c6be6d4e1256022bab29d4
Pearly <135042007+Pmalapile07@users.noreply.github.com>
2026-05-18 21:07:20 +0200

    Implement hash-based routing support
    Add catch-all route to serve index.html for hash-based routing.

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname)));
 
+// This single line fixes hash-based routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
