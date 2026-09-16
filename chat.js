fddb611781238f5d798e47286355f58972f9ae54
Pearly <135042007+Pmalapile07@users.noreply.github.com>
2026-09-17 00:57:50 +0200

    Disable on-screen debug panel

--- chat.js
+++ chat.js
@@ -17,7 +17,7 @@ const SESSION_ID_REGEX        = /^chat-\d{10,16}$/;
 
 // ==================== ON-SCREEN DEBUG PANEL ====================
 const _ScreenDebug = {
-  enabled: true,
+  enabled: false,
   maxRows: 60,
   panel: null,
   logEl: null,
